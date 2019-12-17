923f047a8182a54d4ceff131d8c818c096fc7394
Amelia <wattenberger@gmail.com>
2019-12-17 00:28:34 -0500

    add tools arrows, add d3-array dependency

--- src/core/blocks/tools/ToolsArrowsBlock.js
+++ src/core/blocks/tools/ToolsArrowsBlock.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Block from 'core/blocks/block/Block'
-import ToolsArrowsChart from 'core/charts/tools/ToolsArrowsChart'
+import ToolsArrowsChart from 'core/charts/tools/ToolsArrowsChart/ToolsArrowsChart'
+import { toolsCategories } from '../../../../config/variables.yml'
+import { useI18n } from 'core/i18n/i18nContext'
 
-const ToolsArrowsBlock = ({ block }) => {
+const ToolsArrowsBlock = ({ block, data }) => {
+    const [activeCategory, setActiveCategory] = useState("all")
+
     return (
-        <Block block={block}>
-            <ToolsArrowsChart />
+        <Block
+            titleProps={{ switcher: <Switcher {...{activeCategory, setActiveCategory}} /> }}
+            block={block}>
+            <ToolsArrowsChart {...{data, activeCategory}} />
         </Block>
     )
 }
@@ -19,3 +25,31 @@ ToolsArrowsBlock.propTypes = {
 }
 
 export default ToolsArrowsBlock
+
+
+const categoryOptions = [
+    "all",
+    ...Object.keys(toolsCategories),
+]
+const Switcher = ({ setActiveCategory, activeCategory }) => {
+    const { translate } = useI18n()
+    return (
+        <div className="ChartUnitsSelector">
+            <span className="ButtonGroup">
+                {categoryOptions.map(category => (
+                    <span
+                        key={category}
+                        className={`Button Button--small Button--${
+                            activeCategory === category ? 'active' : 'disabled'
+                        }`}
+                        onClick={() => setActiveCategory(category)}
+                    >
+                        {translate(
+                            category == "all" ? "all" : `page.${category}`
+                        )}
+                    </span>
+                ))}
+            </span>
+        </div>
+    )
+}
---
+++ src/core/charts/tools/ToolsArrowsChart/ToolsArrowsChart.js
@@ -0,0 +1,252 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+import { scaleLinear } from 'd3-scale'
+import map from "lodash/map"
+import { extent, max, sum } from 'd3-array'
+import { toolsCategories } from '../../../../../config/variables.yml'
+import offsets from "./toolsArrowsLabelOffsets.json"
+import { getColor } from 'core/constants.js'
+
+import "./ToolsArrowsChart.scss"
+
+let toolToCategoryMap = {}
+map(toolsCategories, (tools, category) => {
+    tools.forEach(tool => {
+        toolToCategoryMap[tool] = category
+    })
+})
+
+const margins = {
+    marginTop: 20,
+    marginRight: 20,
+    marginBottom: 20,
+    marginLeft: 20,
+}
+const ToolsArrowsChart = ({ data, activeCategory }) => {
+    const [ref, dms] = useChartDimensions(margins, true)
+
+    const tools = data.map(d => d.id)
+    let toolNames = {}
+    data.forEach(tool => {
+        toolNames[tool.id] = tool.entity.name
+    })
+
+    const points = useMemo(() => (
+        data.map(tool => (
+            ["lastYear", "thisYear"].map(year => {
+                const buckets = (tool.experience[year] || {}).buckets || []
+                const points = buckets.map(status => (
+                    conditionDiffs[status.id].map(d => (
+                        d * status.percentage
+                    ))
+                ))
+                return [
+                    sum(points.map(d => d[0])),
+                    sum(points.map(d => d[1])),
+                ]
+            })
+        ))
+    ), [data])
+
+    const scales = useMemo(() => {
+        const xExtent = extent(points.flat().map(d => d[0]))
+        const maxAbsX = max(xExtent.map(Math.abs))
+        const xScale = scaleLinear()
+            .domain([-maxAbsX, maxAbsX])
+            .range([0, dms.boundedWidth])
+
+        const yExtent = extent(points.flat().map(d => d[1]))
+        const maxAbsY = max(yExtent.map(Math.abs))
+        const yScale = scaleLinear()
+            .domain([-maxAbsY, maxAbsY])
+            .range([dms.boundedHeight, 0])
+
+        return {
+            x: xScale,
+            y: yScale,
+        }
+    }, [points, dms])
+
+    return (
+        <div ref={ref} className="ToolsArrowsChart">
+            <svg className="ToolsArrowsChart__svg" height={dms.height} width={dms.width}>
+                <defs>
+                    <path
+                        id="ToolsArrowsChart__arrow"
+                        d="M 0 -9 L 4 0 L -4 0 Z"
+                    />
+                </defs>
+
+                <g transform={`translate(${dms.marginLeft}, ${dms.marginTop})`}>
+                    <line
+                        className="ToolsArrowsChart__axis"
+                        x2={dms.boundedWidth}
+                        y1={dms.boundedHeight / 2}
+                        y2={dms.boundedHeight / 2}
+                    />
+                    <line
+                        className="ToolsArrowsChart__axis"
+                        x1={dms.boundedWidth / 2}
+                        x2={dms.boundedWidth / 2}
+                        y2={dms.boundedHeight}
+                    />
+                    <text
+                        className="ToolsArrowsChart__axis__label"
+                        y={dms.boundedHeight / 2 - 10}>
+                        dislike
+                    </text>
+                    <text
+                    className="ToolsArrowsChart__axis__label"
+                        x={dms.boundedWidth}
+                        y={dms.boundedHeight / 2 - 10}
+                        style={{
+                            textAnchor: "end"
+                        }}>
+                        like
+                    </text>
+                    <text
+                        className="ToolsArrowsChart__axis__label"
+                        x={dms.boundedWidth / 2}
+                        y={10}
+                        style={{
+                            textAnchor: "middle"
+                        }}>
+                        have tried
+                    </text>
+                    <text
+                        className="ToolsArrowsChart__axis__label"
+                        x={dms.boundedWidth / 2}
+                        y={dms.boundedHeight - 10}
+                        style={{
+                            textAnchor: "middle"
+                        }}>
+                        have not tried
+                    </text>
+
+                    {points.map((points, i) => {
+                        const tool = tools[i]
+                        const toolName = toolNames[tool]
+                        const category = toolToCategoryMap[tool]
+                        if (activeCategory != "all" && activeCategory != category) return
+
+                        const [lastYearPoint, thisYearPoint] = points
+                        const x1 = scales.x(lastYearPoint[0] || thisYearPoint[0])
+                        const x2 = scales.x(thisYearPoint[0])
+                        const y1 = scales.y(lastYearPoint[1] || thisYearPoint[1])
+                        const y2 = scales.y(thisYearPoint[1])
+                        const xDiff = x2 - x1
+                        const yDiff = y2 - y1
+                        const angle = Math.atan2(yDiff, xDiff) * 180 / Math.PI + 90
+                        const color = getColor(category)
+
+                        return (
+                            <g>
+                                <line
+                                    className="ToolsArrowsChart__path"
+                                    key={i}
+                                    x1={x2}
+                                    x2={x1}
+                                    y1={y2}
+                                    y2={y1}
+                                    stroke={color}
+                                    title={toolName}
+                                />
+                                <use
+                                    href="#ToolsArrowsChart__arrow"
+                                    className="ToolsArrowsChart__arrow"
+                                    x={x2}
+                                    y={y2}
+                                    style={{
+                                    transformOrigin: `${x2}px ${y2}px`
+                                    }}
+                                    transform={`rotate(${angle})`}
+                                    fill={color}
+                                    title={toolName}
+                                />
+                                <text
+                                    className="ToolsArrowsChart__label"
+                                    fill={color}
+                                    transform={`translate(${
+                                        x2 + ((offsets[tools[i]] || {}).x || 0)
+                                    }, ${
+                                        y2 + ((offsets[tools[i]] || {}).y || 0)
+                                    })`}
+                                >
+                                    { toolName }
+                                </text>
+                            </g>
+                        )
+                    })}
+                </g>
+            </svg>
+        </div>
+    )
+}
+
+ToolsArrowsChart.propTypes = {
+  // ...
+}
+
+export default ToolsArrowsChart
+
+const conditionDiffs = {
+    never_heard: [0, 0],
+    not_interested: [-1, -1],
+    interested: [1, -1],
+    would_not_use: [-1, 1],
+    would_use: [1, 1],
+}
+
+export const combineChartDimensions = dimensions => {
+    let parsedDimensions = {
+        marginTop: 0,
+        marginRight: 0,
+        marginBottom: 0,
+        marginLeft: 0,
+        ...dimensions,
+    }
+
+    return {
+        ...parsedDimensions,
+        boundedHeight: Math.max(parsedDimensions.height - parsedDimensions.marginTop - parsedDimensions.marginBottom, 0),
+        boundedWidth: Math.max(parsedDimensions.width - parsedDimensions.marginLeft - parsedDimensions.marginRight, 0),
+    }
+  }
+
+  export const useChartDimensions = (passedSettings, isSquare=false) => {
+    const ref = useRef()
+    const dimensions = combineChartDimensions(passedSettings)
+
+    const [width, changeWidth] = useState(0)
+    const [height, changeHeight] = useState(0)
+
+    useEffect(() => {
+        if (dimensions.width && dimensions.height) return [ref, dimensions]
+
+        const element = ref.current
+        const onResize = () => {
+            if (!element) return
+
+            const dimensions = element.getBoundingClientRect()
+
+            if (width !== dimensions.width) {
+                changeWidth(dimensions.width)
+                if (isSquare) {
+                    changeHeight(dimensions.width)
+                }
+            }
+            if (!isSquare && height !== dimensions.height) changeHeight(dimensions.height)
+        }
+        window.addEventListener("resize", onResize)
+        setTimeout(onResize, 100)
+
+        return () => window.removeEventListener("resize", onResize)
+    }, [dimensions, height, width])
+
+    const newSettings = combineChartDimensions({
+        ...dimensions,
+        width: dimensions.width || width,
+        height: dimensions.height || height,
+    })
+
+    return [ref, newSettings]
+}
