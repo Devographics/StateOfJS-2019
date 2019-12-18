66dc14c684d7408ba078fde170e76abfcd0fa097
Raphaël Benitte <501642+plouc@users.noreply.github.com>
2019-12-18 20:03:39 +0900

    Add percentages to ToolsExperienceRankingChart

--- src/core/charts/tools/ToolsExperienceRankingChart.js
+++ src/core/charts/tools/ToolsExperienceRankingChart.js
@@ -2,7 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { ResponsiveBump } from '@nivo/bump'
 import theme from 'nivoTheme'
+import { colors } from 'core/constants'
 // import get from 'lodash/get'
+
+const CustomPoint = props => {
+    const { x, y, data, isInactive, size, color, borderColor, borderWidth } = props
+
+    return (
+        <g transform={`translate(${x}, ${y})`} style={{ pointerEvents: 'none' }}>
+            <circle r={(size + borderWidth) / 2} cy={size / 5} fill="rgba(0, 0, 0, .2)" />
+            <circle
+                r={size / 2}
+                fill={colors.greyDarker}
+                stroke={borderColor}
+                strokeWidth={borderWidth}
+            />
+            {!isInactive && (
+                <text textAnchor="middle" y={4} fill={color} fontSize="11px">
+                    {Math.round(data.percentage)}%
+                </text>
+            )}
+        </g>
+    )
+}
 
 const ToolsExperienceRankingChart = ({ data }) => {
     // const formatTick = id => {
@@ -15,8 +37,6 @@ const ToolsExperienceRankingChart = ({ data }) => {
             data={data}
             margin={{ top: 40, right: 100, bottom: 40, left: 100 }}
             colors={{ scheme: 'accent' }}
-            lineWidth={5}
-            activeLineWidth={8}
             inactiveLineWidth={5}
             theme={theme}
             enableGridX={true}
@@ -43,9 +63,17 @@ const ToolsExperienceRankingChart = ({ data }) => {
                 modifiers: [['brighter', 1]]
             }}
             endLabelPadding={20}
-            pointSize={12}
-            activePointSize={20}
-            inactivePointSize={12}
+            pointComponent={CustomPoint}
+            pointColor={{ from: 'serie.color', modifiers: [['brighter', 2]] }}
+            lineWidth={5}
+            pointSize={36}
+            pointBorderWidth={3}
+            pointBorderColor={{ from: 'serie.color' }}
+            activeLineWidth={8}
+            activePointSize={42}
+            activePointBorderWidth={4}
+            inactivePointSize={0}
+            inactivePointBorderWidth={2}
         />
     )
 }
